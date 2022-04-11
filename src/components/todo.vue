ab3809b8ebd0afa29b93979f48fa80d946d27e1a
nisrinemr1 <nisrinemr1@gmail.com>
2022-04-11 15:19:27 +0200

    style of the form from parent moved to children

--- src/components/todo.vue
+++ src/components/todo.vue
@@ -143,6 +143,7 @@ import todoForm from './todo/todo-form.vue'
   .content {
     height: 100vh;
     width: 100vw;
+    padding-bottom: 400px;
     display: flex;
     align-items: center;
     flex-direction: column;
@@ -268,74 +269,6 @@ import todoForm from './todo/todo-form.vue'
 
       &.false{
         height: 0;
-      }
-    }
-    .form {
-      overflow: hidden;
-      position: fixed;
-      overflow-y: scroll;
-      bottom: 0;
-      left: 0;
-      width: 100vw;
-      display: flex;
-      justify-content: end;
-      flex-direction: row-reverse;
-      background-color: white;
-      padding-top: 20px;
-      padding-bottom: 30px;
-
-      .close-form-btn {
-        margin-bottom: 60px;
-        margin-right: 40px;
-        border: none;
-        font-size: 1.1rem;
-        background-color: white;
-      }
-
-      form {
-        padding-top: 40px;
-
-        label {
-          padding-right: 15px;
-        }
-
-        input {
-          margin-right: 15px;
-          border-radius: 20px;
-          background-color: rgb(253, 197, 207);
-          border: none;
-          padding-top: 5px;
-          padding-bottom: 5px;
-          padding-left: 15px;
-          box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
-        }
-
-        select {
-          margin-right: 15px;
-          padding-top: 5px;
-          padding-bottom: 5px;
-          padding-left: 5px;
-          padding-right: 5px;
-          background-color: rgb(253, 197, 207);
-          border: none;
-          border-radius: 20px;
-          box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
-        }
-
-        button {
-          padding-top: 10px;
-          padding-bottom: 10px;
-          padding-left: 10px;
-          padding-right: 10px;
-          background-color: rgb(253, 197, 207);
-          border: none;
-          border-radius: 20px;
-          box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
-        }
-
-        button:hover {
-          background-color: rgb(254, 212, 220);
-        }
       }
     }
   }
--- src/components/todo/todo-form.vue
+++ src/components/todo/todo-form.vue
@@ -41,10 +41,10 @@
             list: Array
         },
 
-        data(){
-            return{
+        data() {
+            return {
                 /* **** HIDE FORM **** */
-                hide:"",
+                hide: "",
 
                 /* **** TODO OBJECT **** */
                 todo: {
@@ -53,7 +53,7 @@
                 },
             }
         },
-        
+
 
         methods: {
             /* **** CLOSE FORM METHODS ON CLICK **** */
@@ -70,7 +70,7 @@
                 e.preventDefault()
 
                 /*  ALERT WHEN THE INPUT IS EMPTY  */
-                if(!this.todo.name){
+                if (!this.todo.name) {
                     alert('Ajouter une tâche')
                     /* TODO a demanader  */
                     return
@@ -83,7 +83,9 @@
                 })
 
                 /* TO SEND THE LIST UPDATE TO THE PARENT */
-                this.$emit('listUpdate', {...this.list})
+                this.$emit('listUpdate', {
+                    ...this.list
+                })
 
                 /* TO CLEAR THE FORM */
                 this.todo.name = ''
@@ -94,5 +96,72 @@
 </script>
 
 <style lang="scss" scoped>
+    .form {
+        overflow: hidden;
+        position: fixed;
+        overflow-y: scroll;
+        bottom: 0;
+        left: 0;
+        width: 100vw;
+        display: flex;
+        justify-content: end;
+        flex-direction: row-reverse;
+        background-color: white;
+        padding-top: 20px;
+        padding-bottom: 30px;
 
+        .close-form-btn {
+            margin-bottom: 60px;
+            margin-right: 40px;
+            border: none;
+            font-size: 1.1rem;
+            background-color: white;
+        }
+
+        form {
+            padding-top: 40px;
+
+            label {
+                padding-right: 15px;
+            }
+
+            input {
+                margin-right: 15px;
+                border-radius: 20px;
+                background-color: rgb(253, 197, 207);
+                border: none;
+                padding-top: 5px;
+                padding-bottom: 5px;
+                padding-left: 15px;
+                box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+            }
+
+            select {
+                margin-right: 15px;
+                padding-top: 5px;
+                padding-bottom: 5px;
+                padding-left: 5px;
+                padding-right: 5px;
+                background-color: rgb(253, 197, 207);
+                border: none;
+                border-radius: 20px;
+                box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+            }
+
+            button {
+                padding-top: 10px;
+                padding-bottom: 10px;
+                padding-left: 10px;
+                padding-right: 10px;
+                background-color: rgb(253, 197, 207);
+                border: none;
+                border-radius: 20px;
+                box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+            }
+
+            button:hover {
+                background-color: rgb(254, 212, 220);
+            }
+        }
+    }
 </style>
