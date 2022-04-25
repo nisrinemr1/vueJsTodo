26f9c771075b73907082dfcb7aa51d591b82d32e
nisrine-mrn <nisrinemr1@gmail.com>
2022-04-25 11:45:36 +0200

    UI - Sing-in Sing-up added

---
+++ src/assets/svg/dynamic/flags/nl.vue
@@ -0,0 +1,14 @@
+<template>
+  <svg width="100%" height="100%" x="0" y="0" viewBox="0 0 512 512">
+    <g>
+      <circle xmlns="http://www.w3.org/2000/svg" cx="256" cy="256" fill="#f0f0f0" r="256" data-original="#f0f0f0"
+       ></circle>
+      <path xmlns="http://www.w3.org/2000/svg"
+        d="m256 0c-110.071 0-203.906 69.472-240.077 166.957h480.155c-36.172-97.485-130.007-166.957-240.078-166.957z"
+        fill="#a2001d" data-original="#a2001d"></path>
+      <path xmlns="http://www.w3.org/2000/svg"
+        d="m256 512c110.071 0 203.906-69.472 240.077-166.957h-480.154c36.171 97.485 130.006 166.957 240.077 166.957z"
+        fill="#0052b4" data-original="#0052b4"></path>
+    </g>
+  </svg>
+</template>
