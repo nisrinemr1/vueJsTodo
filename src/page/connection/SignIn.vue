e45f475d2f0df54bc99c3a7ea7a938004b004838
nisrine-mrn <nisrinemr1@gmail.com>
2022-04-26 12:59:07 +0200

    Comments added

--- src/page/connection/SignIn.vue
+++ src/page/connection/SignIn.vue
@@ -1,6 +1,15 @@
 <template>
+
+
+    <!-- ******************************  SING IN PAGE  ****************************** -->
+    <!-- ******************************  SING IN PAGE  ****************************** -->
+    <!-- ******************************  SING IN PAGE  ****************************** -->
     <div id="sign-in-page">
 
+
+        <!-- *************************  HEADER  ************************* -->
+        <!-- *************************  HEADER  ************************* -->
+        <!-- *************************  HEADER  ************************* -->
         <header>
 
             <h1>Jobii</h1>
@@ -8,25 +17,64 @@
         </header>
 
 
+        <!-- *************************  SIGN IN CONTAINER  ************************* -->
+        <!-- *************************  SIGN IN CONTAINER  ************************* -->
+        <!-- *************************  SIGN IN CONTAINER  ************************* -->
         <div class="sing-in-container">
 
             <div class="sing-in">
+
+                <!-- ******************** TITLE ******************** -->
+                <!-- ******************** TITLE ******************** -->
+                <!-- ******************** TITLE ******************** -->
                 <h1>Se connecter</h1>
 
+
+                <!-- ******************** SIGN IN WITH GOOGLE ******************** -->
+                <!-- ******************** SIGN IN WITH GOOGLE ******************** -->
+                <!-- ******************** SIGN IN WITH GOOGLE ******************** -->
                 <button @click.prevent="GoogleSingIn"> Google </button>
 
+
+                <!-- ******************** OR ******************** -->
+                <!-- ******************** OR ******************** -->
+                <!-- ******************** OR ******************** -->
                 <p>ou</p>
 
+
+                <!-- ******************** EMAIL ******************** -->
+                <!-- ******************** EMAIL ******************** -->
+                <!-- ******************** EMAIL ******************** -->
                 <input v-model="emailSignIn" type="email" placeholder="Email">
+
+
+                <!-- ******************** PASSWORD ******************** -->
+                <!-- ******************** PASSWORD ******************** -->
+                <!-- ******************** PASSWORD ******************** -->
                 <input v-model="passwordSignIn" type="password" placeholder="Mot de passe">
 
+
+                <!-- ******************** CONNECTION BTN ******************** -->
+                <!-- ******************** CONNECTION BTN ******************** -->
+                <!-- ******************** CONNECTION BTN ******************** -->
                 <div class="connection">
 
+                     <!-- *************** FORGOT PASSWORD *************** -->
+                     <!-- *************** FORGOT PASSWORD *************** -->
+                     <!-- *************** FORGOT PASSWORD *************** -->
                     <p>Mot de passe oublié?</p>
+
+
+                    <!-- *************** SIGN-IN BTN *************** -->
+                    <!-- *************** SIGN-IN BTN *************** -->
+                    <!-- *************** SIGN-IN BTN *************** -->
                     <button @click.prevent="signIn">Se connecter</button>
 
                 </div>
 
+                <!-- ******************** DON'T HAVE AN ACCOUNT YET? ******************** -->
+                <!-- ******************** DON'T HAVE AN ACCOUNT YET? ******************** -->
+                <!-- ******************** DON'T HAVE AN ACCOUNT YET? ******************** -->
                 <p>Vous n'avez pas de compte? <router-link class="nav-link" :to="{ name : 'signUp' }">Inscrivez-vous maintenant!</router-link> </p>
 
             </div>
@@ -35,8 +83,10 @@
 </template>
 
 <script>
+    //IMPORT AUTH AND FUNCTIONS FROM INIT.JS
     import { auth } from "../../firebase/init"
     import { signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider } from "firebase/auth"
+
     export default {
         name: 'singIn',
 
@@ -48,7 +98,11 @@
         },
 
         methods:{
+
+            //TO SIGN IN WITH THE CREATED AUTHENTIFICATION (EMAIL)
+            //TO SIGN IN WITH THE CREATED AUTHENTIFICATION (EMAIL)
             signIn(){
+
                 signInWithEmailAndPassword(auth, this.emailSignIn, this.passwordSignIn).then(
                     function(user){
                         alert('Vous êtes connecté!')
@@ -59,6 +113,10 @@
                     }
                 )
             },
+
+
+            //TO SIGN IN WITH THE CREATED AUTHENTIFICATION (GMAIL)
+            //TO SIGN IN WITH THE CREATED AUTHENTIFICATION (GMAIL)
             GoogleSingIn() {
                 const provider = new GoogleAuthProvider();
                     signInWithPopup(auth, provider).then((result) => {
@@ -83,6 +141,8 @@
         width: 100%;
         height: 100vw;
 
+        //*************************  HEADER  *************************
+        //*************************  HEADER  *************************
         header {
 
             border-left: 2px solid black;
@@ -93,6 +153,9 @@
 
             margin-top: 0;
 
+
+            //********************  LOGO JOBII  ********************
+            //********************  LOGO JOBII  ********************
             h1 {
                 margin-top: 0;
                 margin-bottom: 0;
@@ -106,7 +169,8 @@
         }
 
 
-
+        //*************************  SIGN IN CONTAINER  *************************
+        //*************************  SIGN IN CONTAINER  *************************
         .sing-in-container {
 
             width: 100%;
@@ -126,16 +190,16 @@
                 flex-direction: column;
                 padding-top: 113px;
                 padding-bottom: 133px;
-
+                
+                //******************** TITLE ********************
+                //******************** TITLE ********************
                 h1 {
                     width: 100%;
                 }
             }
-
-            p{
-                text-align: center;
-            }
-
+            
+            //*************** SIGN-IN BTN ***************
+            //*************** SIGN-IN BTN ***************
             .connection{
                 padding-top: 20px;
 
@@ -143,6 +207,11 @@
                 justify-content: space-between;
             }
 
+            //******************** DON'T HAVE AN ACCOUNT YET? ********************
+            //******************** DON'T HAVE AN ACCOUNT YET? ********************
+            p{
+                text-align: center;
+            }
         }
 
     }
--- src/page/connection/SignUp.vue
+++ src/page/connection/SignUp.vue
@@ -1,42 +1,82 @@
 <template>
+
+    <!-- ******************************  SING UP PAGE  ****************************** -->
+    <!-- ******************************  SING UP PAGE  ****************************** -->
+    <!-- ******************************  SING UP PAGE  ****************************** -->
     <div id="sign-up-page">
 
+        <!-- *************************  HEADER  ************************* -->
+        <!-- *************************  HEADER  ************************* -->
+        <!-- *************************  HEADER  ************************* -->
         <header>
-
             <h1>Jobii</h1>
-
         </header>
 
+        
+        <!-- *************************  SIGN UP CONTAINER  ************************* -->
+        <!-- *************************  SIGN UP CONTAINER  ************************* -->
+        <!-- *************************  SIGN UP CONTAINER  ************************* -->
+        <div class="sing-up-container">
 
-        <div class="sing-in-container">
+            <div class="sing-up">
 
-            <div class="sing-in">
+                <!-- ******************** TITLE ******************** -->
+                <!-- ******************** TITLE ******************** -->
+                <!-- ******************** TITLE ******************** -->
                 <h1>S'inscrire</h1>
 
+                <!-- ******************** SIGN UP WITH GOOGLE ******************** -->
+                <!-- ******************** SIGN UP WITH GOOGLE ******************** -->
+                <!-- ******************** SIGN UP WITH GOOGLE ******************** -->
                 <button @click.prevent="GoogleSingUp"> Google </button>
 
+
+                <!-- ******************** OR ******************** -->
+                <!-- ******************** OR ******************** -->
+                <!-- ******************** OR ******************** -->
                 <p>ou</p>
 
+
+                <!-- ******************** FIRST NAME ******************** -->
+                <!-- ******************** FIRST NAME ******************** -->
+                <!-- ******************** FIRST NAME ******************** -->
                 <input v-model="firstNameSignUp" type="text" placeholder="Prénom">
+
+
+                <!-- ******************** LAST NAME ******************** -->
+                <!-- ******************** LAST NAME ******************** -->
+                <!-- ******************** LAST NAME ******************** -->
                 <input v-model="lastNameSingUp" type="text" placeholder="Nom de famille">
 
+
+                <!-- ******************** EMAIL ******************** -->
+                <!-- ******************** EMAIL ******************** -->
+                <!-- ******************** EMAIL ******************** -->
                 <input v-model="emailSignUp" type="email" placeholder="Email">
+
+
+                <!-- ******************** PASSWORD ******************** -->
+                <!-- ******************** PASSWORD ******************** -->
+                <!-- ******************** PASSWORD ******************** -->
                 <input v-model="passwordSignUp" type="password" placeholder="Mot de passe">
 
 
+                <!-- ******************** CONNECTION BTN ******************** -->
+                <!-- ******************** CONNECTION BTN ******************** -->
+                <!-- ******************** CONNECTION BTN ******************** -->
                 <div class="connection">
-
                     <button @click.prevent="signUp">S'inscrire</button>
-
                 </div>
 
+
+                <!-- ******************** ALREADY HAVE AN ACCOUNT? ******************** -->
+                <!-- ******************** ALREADY HAVE AN ACCOUNT? ******************** -->
+                <!-- ******************** ALREADY HAVE AN ACCOUNT? ******************** -->
                 <p>Vous avez déjà un compte? <router-link class="nav-link" :to="{ name : 'signIn' }">Se connecter
                     </router-link>
                 </p>
 
-
             </div>
-
 
         </div>
 
@@ -44,16 +84,29 @@
     </div>
 </template>
 
+
+
 <script>
+
+    //******************** IMPORT AUTHENTIFICATIONS FROM INI.JS ********************
+    //******************** IMPORT AUTHENTIFICATIONS FROM INI.JS ********************
     import {
         auth,
         db
     } from "../../firebase/init"
+
+
+    //******************** IMPORT FROM AUTH (EMAIL, GOOGLE) ********************
+    //******************** IMPORT FROM AUTH (EMAIL, GOOGLE) ********************
     import {
         createUserWithEmailAndPassword,
         signInWithPopup,
         GoogleAuthProvider
     } from "firebase/auth"
+
+
+    //******************** IMPORT FIRESTORE ********************
+    //******************** IMPORT FIRESTORE ********************
     import {
         doc,
         setDoc
@@ -74,40 +127,56 @@
 
         methods: {
 
-            signUp() {
-                createUserWithEmailAndPassword(auth, this.emailSignUp, this.passwordSignUp)
-                    .then((data) => {
-                        let uid = data.user.uid
-                        alert('Votre compte à été créer!')
-                        console.log(data.user.uid)
-                        this.createUser(uid, this.emailSignUp, this.firstNameSignUp, this.lastNameSingUp)
-                    }).catch((error) => {
-                        const errorCode = error.code;
-                        const errorMessage = error.message;
-                    })
-            },
-
+            //******************** CREATING USER DATA TO FIRESTORE ********************
+            //******************** CREATING USER DATA TO FIRESTORE ********************
             async createUser(uid, email, firstname, lastname) {
+                
                 await setDoc(doc(db, "users", uid), {
                     firstname: firstname,
                     lastname: lastname,
                     email: email
                 })
+
             },
 
-            GoogleSingUp() {
-                const provider = new GoogleAuthProvider();
 
+            //******************** ADDING USER TO FIREBASE AUTH WITH EMAIL ********************
+            //******************** ADDING USER TO FIREBASE AUTH WITH EMAIL ********************
+            signUp() {
+                
+                createUserWithEmailAndPassword(auth, this.emailSignUp, this.passwordSignUp)
+                    //PROMISE
+                    .then((data) => {
+                        // GET THE USER ID FROM THE INSPECTOR : console.log(user)
+                        let uid = data.user.uid
+
+                        alert('Votre compte à été créer!')
+
+                        //TO SEND THE USER IN DATA
+                        this.createUser(uid, this.emailSignUp, this.firstNameSignUp, this.lastNameSingUp)
+
+                    }).catch((error) => { //IF ERROR
+                        const errorCode = error.code;
+                        const errorMessage = error.message;
+                    })
+            },
+            
+            
+            //******************** ADDING INFORMATIONS TO FIRESTORE WITH GOOGLE ********************
+            //******************** ADDING INFORMATIONS TO FIRESTORE WITH GOOGLE ********************
+            GoogleSingUp() {
+                
+                const provider = new GoogleAuthProvider();
+                
                 signInWithPopup(auth, provider).then((result) => {
+                        //GETTING THE USER INFORMATION FROM THE DATA: console.log(user)
                         let uid = result.user.uid
                         let email = result.user.email
                         let firstname = result.user.displayName.split(' ')[0]
                         let lastname = result.user.displayName.split(' ')[1]
-
+                        
+                        // TO ADD THE USER TO THE FIRESOTRE
                         this.createUser(uid, email, firstname, lastname)
-
-                        console.log(uid, email, firstname, lastname)
-                        console.log(result)
                     })
                     .catch((error) => {
                         const errorCode = error.code;
@@ -121,11 +190,17 @@
 </script>
 
 <style lang="scss">
+
+    //******************************  SING UP PAGE  ******************************
+    //******************************  SING UP PAGE  ******************************
     #sign-up-page {
 
         width: 100%;
         height: 100vw;
 
+
+        //*************************  HEADER  *************************
+        //*************************  HEADER  *************************
         header {
 
             border-left: 2px solid black;
@@ -135,7 +210,9 @@
             margin-left: 50px;
 
             margin-top: 0;
-
+            
+            //******************** TITLE ******************** 
+            //******************** TITLE ******************** 
             h1 {
                 margin-top: 0;
                 margin-bottom: 0;
@@ -149,8 +226,9 @@
         }
 
 
-
-        .sing-in-container {
+        //*************************  SIGN UP CONTAINER  *************************
+        //*************************  SIGN UP CONTAINER  *************************
+        .sing-up-container {
 
             width: 100%;
 
@@ -162,7 +240,7 @@
             padding-top: 113px;
             padding-bottom: 133px;
 
-            .sing-in {
+            .sing-up {
                 width: 100%;
                 max-width: 513px;
                 display: flex;
@@ -175,10 +253,14 @@
                 }
             }
 
+            //******************** ALREADY HAVE AN ACCOUNT? ********************
+            //******************** ALREADY HAVE AN ACCOUNT? ********************
             p {
                 text-align: center;
             }
-
+            
+            //******************** CONNECTION BTN ********************
+            //******************** CONNECTION BTN ********************
             .connection {
                 padding-top: 20px;
 
